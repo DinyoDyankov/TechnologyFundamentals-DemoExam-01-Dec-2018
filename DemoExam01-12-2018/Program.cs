ade53baba2ca06c9309074484f04fcf8e66b4827
Calabria88 <Calabria88@students.softuni.bg>
2019-05-01 11:57:38 +0300

    Add project files.

---
+++ DemoExam01-12-2018/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DemoExam01_12_2018
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string messageToDecrypt = Console.ReadLine();
+            string[] replacementString = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            string decryptedMessage = string.Empty;
+
+            var regex = @"[d-z{}|#]";
+
+            var matches = Regex.Matches(messageToDecrypt, regex);
+
+            if (matches.Count != messageToDecrypt.Length)
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+            else
+            {
+                for (int i = 0; i < messageToDecrypt.Length; i++)
+                {
+                    char currentElementToDecrypt = messageToDecrypt[i];
+
+                    int currentE = currentElementToDecrypt - 3;
+                    char decryptedElement = (char)currentE;
+
+                    decryptedMessage += decryptedElement;
+                }
+
+                if (decryptedMessage.Contains(replacementString[0]))
+                {
+                    decryptedMessage = decryptedMessage.Replace(replacementString[0], replacementString[1]);
+                }
+
+                Console.WriteLine(decryptedMessage);
+            }
+        }
+    }
+}
---
+++ P01.Dictionary/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace P01.Dictionary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> wordsAndDefinitionsInput = Console.ReadLine()
+                .Split(new[] { " | " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Dictionary<string, List<string>> libaryOfWordsAndTheirDefinitions = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < wordsAndDefinitionsInput.Count; i++)
+            {
+                string[] currentWordAndHerDefinitions = wordsAndDefinitionsInput[i].Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (libaryOfWordsAndTheirDefinitions.ContainsKey(currentWordAndHerDefinitions[0]))
+                {
+                    libaryOfWordsAndTheirDefinitions[currentWordAndHerDefinitions[0]].Add(currentWordAndHerDefinitions[1]);
+                }
+                else
+                {
+                    List<string> currentList = new List<string>();
+                    currentList.Add(currentWordAndHerDefinitions[1]);
+
+                    libaryOfWordsAndTheirDefinitions.Add(currentWordAndHerDefinitions[0], currentList);
+                }
+            }
+
+            string [] wordsToCheck = Console.ReadLine()
+                .Split(new[] { " | " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int i = 0; i < wordsToCheck.Length; i++)
+            {
+                foreach (string word in libaryOfWordsAndTheirDefinitions.Keys)
+                {
+                    if (word == wordsToCheck[i])
+                    {
+                        Console.WriteLine(word);
+                        foreach (var def in libaryOfWordsAndTheirDefinitions[word].OrderByDescending(d => d.Length))
+                        {
+                            Console.WriteLine($" -{def}");
+                        }
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "List")
+            {
+                foreach (var word in libaryOfWordsAndTheirDefinitions.Keys.OrderBy(w => w))
+                {
+                    Console.Write(word + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
